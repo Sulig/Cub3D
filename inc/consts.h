c71a6cdef9ad02b4a78b72ec5d59a25fdb559107
Sasha Domínguez Ferrer <sadoming@student.42barcelona.com>
2024-10-21 19:00:46 +0200

    Including Consts

---
+++ inc/consts.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   consts.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadoming <sadoming@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 16:49:31 by sadoming          #+#    #+#             */
+/*   Updated: 2024/10/21 17:50:19 by sadoming         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONSTS_H
+# define CONSTS_H
+
+/* PATH TO TEXTURES */
+# define TX_ERROR	"./assets/textures/png/error-tile.png"
+# define TX_ERA		"./assets/textures/png/error-tile_ALPHA.png"
+# define TX_ERR		"./assets/textures/png/error-tile_RED.png"
+# define TX_ERG		"./assets/textures/png/error-tile_GREEN.png"
+# define TX_ERC		"./assets/textures/png/error-tile_CYAN.png"
+# define TX_ERY		"./assets/textures/png/error-tile_YELOW.png"
+# define TX_ERGR	"./assets/textures/png/error-tile_GREY.png"
+
+
+/* THE TEXTURES TO USE */
+# define NO TX_ERR
+# define EA TX_ERG
+# define WE TX_ERROR
+# define SO TX_ERA
+/* ----------------- */
+
+#endif
