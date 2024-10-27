5ce0ed3d35fd0f93e13d3b85c14d365d54d725cc
Andrea Martins <andmart2@car3s4.42barcelona.com>
2024-10-27 19:02:06 +0100

    format

--- inc/cub3D.h
+++ inc/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: andmart2 <andmart2@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 17:42:58 by andmart2          #+#    #+#             */
-/*   Updated: 2024/10/27 18:49:28 by andmart2         ###   ########.fr       */
+/*   Updated: 2024/10/27 19:01:27 by andmart2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,34 +24,34 @@
 
 typedef struct s_ray
 {
-    double  camera_x;
-    double  dir_x;
-    double  dir_y;
-    double  side_x;
-    double  side_y;
-    double  wall_dist;
-    double  wall_x;
-    int     side;
-    int     map_x;
-    int     map_y;
-    int     step_x;
-    int     step_y;
-    int     line_h;
-    int     draw_start;
-    int     draw_end;
-}       t_ray;
+	double	camera_x;
+	double	dir_x;
+	double	dir_y;
+	double	side_x;
+	double	side_y;
+	double	wall_dist;
+	double	wall_x;
+	int		side;
+	int		map_x;
+	int		map_y;
+	int		step_x;
+	int		step_y;
+	int		line_h;
+	int		draw_start;
+	int		draw_end;
+}			t_ray;
 
 typedef struct s_player
 {
-    char    dir;
-    double  pos_x;
-    double  pos_y;
-    double  dir_x;
-    double  dir_y;
-    double  plane_x;
-    double  plane_y;
-    int     rotate;
-    int     move_x;
-    int     move_y;
-    int     has_moved;
-}       t_player
+	char	dir;
+	double	pos_x;
+	double	pos_y;
+	double	dir_x;
+	double	dir_y;
+	double	plane_x;
+	double	plane_y;
+	int		rotate;
+	int		move_x;
+	int		move_y;
+	int		has_moved;
+}			t_player;
