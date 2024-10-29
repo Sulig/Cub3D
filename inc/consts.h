eacb50dd8b4584ea72f922a43820ba935e14e088
Sasha Domínguez Ferrer <sadoming@student.42barcelona.com>
2024-10-29 14:41:35 +0100

    Let's begin. I'm gonna make you wish that i stayed gone. Tune on in

--- inc/consts.h
+++ inc/consts.h
@@ -6,7 +6,7 @@
 /*   By: sadoming <sadoming@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 16:49:31 by sadoming          #+#    #+#             */
-/*   Updated: 2024/10/22 14:43:47 by sadoming         ###   ########.fr       */
+/*   Updated: 2024/10/29 13:51:21 by sadoming         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,20 @@
 
 /*		 ## TEXTURES ##		*/
 /* ERROR TEXTURES */
-# define TX_ERROR	"./assets/textures/png/error-tile.png"
-# define TX_ERA		"./assets/textures/png/error-tile_ALPHA.png"
-# define TX_ERR		"./assets/textures/png/error-tile_RED.png"
-# define TX_ERG		"./assets/textures/png/error-tile_GREEN.png"
-# define TX_ERC		"./assets/textures/png/error-tile_CYAN.png"
-# define TX_ERY		"./assets/textures/png/error-tile_YELOW.png"
-# define TX_ERGR	"./assets/textures/png/error-tile_GREY.png"
+# define TX_ERROR	"./assets/textures/error-tile-colors/error-tile.png"
+# define TX_ERA		"./assets/textures/error-tile-colors/error-tile_ALPHA.png"
+# define TX_ERR		"./assets/textures/error-tile-colors/error-tile_RED.png"
+# define TX_ERG		"./assets/textures/error-tile-colors/error-tile_GREEN.png"
+# define TX_ERC		"./assets/textures/error-tile-colors/error-tile_CYAN.png"
+# define TX_ERY		"./assets/textures/error-tile-colors/error-tile_YELOW.png"
+# define TX_ERGR	"./assets/textures/error-tile-colors/error-tile_GREY.png"
 /* --- */
+
+/* TEST TEXTURES */
+# define TX_FLOOR	"./assets/textures/so-long/floor-diamond.png"
+# define TX_COLOR	"./assets/textures/so-long/justacolor.png"
+/* --- */
+
 /* THE TEXTURES TO USE */
 # define NO TX_ERR
 # define EA TX_ERG
--- src/cub_main.c
+++ src/cub_main.c
@@ -6,25 +6,85 @@
 /*   By: sadoming <sadoming@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 11:56:10 by sadoming          #+#    #+#             */
-/*   Updated: 2024/10/22 14:52:18 by sadoming         ###   ########.fr       */
+/*   Updated: 2024/10/29 14:38:43 by sadoming         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/game.h"
 
 /* TESTING ZONE! */
+static mlx_image_t *bat, *floor, *wall;
+static int			vel = 7;
+
+static int mapS = 64;
+static int map[]=
+{
+	1,1,1,1,1,1,1,1,
+	1,0,1,0,0,0,0,1,
+	1,0,1,0,0,0,0,1,
+	1,0,1,0,0,0,0,1,
+	1,0,0,0,0,0,0,1,
+	1,0,0,0,1,0,0,1,
+	1,0,0,0,0,0,0,1,
+	1,1,1,1,1,1,1,1
+};
+
 static void error(void)
 {
 	ft_printf_fd(2, mlx_strerror(mlx_errno));
 	exit(EXIT_FAILURE);
 }
 
+void	printmap(mlx_t *mlx)
+{
+	int y = 0, x = 0;
+
+	for (int s = 0; s < mapS; s++)
+	{
+		if (map[s] == 0)
+			if (mlx_image_to_window(mlx, floor, x * mapS, y * mapS) < 0)
+        		error();
+		if (map[s] == 1)
+			if (mlx_image_to_window(mlx, wall, x * mapS, y * mapS) < 0)
+        		error();
+		x++;
+		if ((s + 1) % 8 == 0 && s)
+		{
+			x = 0;
+			y++;
+		}
+	}
+}
+
+/*this function will be called for every frame
+* this is for detecting key_inputs
+*/
 void ft_hook(void* param)
 {
 	mlx_t* mlx = param;
 
 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(mlx);
+	if (mlx_is_key_down(mlx, MLX_KEY_UP))
+	{
+		bat->instances[0].y -= vel;
+		ft_printf_fd(1, "Key pressed: %i\n", MLX_KEY_UP);
+	}
+	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
+	{
+		bat->instances[0].y += vel;
+		ft_printf_fd(1, "Key pressed: %i\n",MLX_KEY_DOWN);
+	}
+	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
+	{
+		bat->instances[0].x -= vel;
+		ft_printf_fd(1, "Key pressed: %i\n",MLX_KEY_LEFT);
+	}
+	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
+	{
+		bat->instances[0].x += vel;
+		ft_printf_fd(1, "Key pressed: %i\n",MLX_KEY_RIGHT);
+	}
 }
 
 void	start(void)
@@ -34,17 +94,37 @@ void	start(void)
 	if (!(mlx = mlx_init(WIDTH, HEIGHT, "CUB3D", true)))
 		error();
 
+	/**/ /**/ /**/ /**/ /**/ /**/ /**/
 	mlx_texture_t* texture = mlx_load_png(BAT);
 	if (!texture)
         error();
-
 	// Convert texture to a displayable image
-	mlx_image_t* img = mlx_texture_to_image(mlx, texture);
-	if (!img)
+	bat = mlx_texture_to_image(mlx, texture);
+	if (!bat)
         error();
+	/**/
+	mlx_texture_t* textur = mlx_load_png(TX_ERGR);
+	if (!textur)
+        error();
+	// Convert texture to a displayable image
+	floor = mlx_texture_to_image(mlx, textur);
+	if (!floor)
+        error();
+	/**/
+	mlx_texture_t* textu = mlx_load_png(TX_ERROR);
+	if (!textu)
+        error();
+	// Convert texture to a displayable image
+	wall = mlx_texture_to_image(mlx, textu);
+	if (!wall)
+        error();
+	/**/ /**/ /**/ /**/ /**/ /**/ /**/
 
-	// Display the image
-	if (mlx_image_to_window(mlx, img, 0, 0) < 0)
+	//print map
+	printmap(mlx);
+
+	// Put bat into window
+	if (mlx_image_to_window(mlx, bat, 232, 232) < 0)
         error();
 
 	mlx_loop_hook(mlx, ft_hook, mlx);
