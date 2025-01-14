d83c64841c848b363ca2f8c76df98e4686965e56
Andrea Martins <andmart2@car10s3.42barcelona.com>
2025-01-14 21:36:58 +0100

    val and normi ok mover 1 funcion en otro archivo clean

--- libft/obj/ft_cnv/ft_strcapitalize.d
+++
@@ -1,1 +0,0 @@
-obj/ft_cnv/ft_strcapitalize.o: src/ft_cnv/ft_strcapitalize.c
--- libft/obj/ft_wrt/ft_printf_fd.d
+++
@@ -1,3 +0,0 @@
-obj/ft_wrt/ft_printf_fd.o: src/ft_wrt/ft_printf_fd.c \
- src/ft_wrt/../../inc/write.h
-src/ft_wrt/../../inc/write.h:
